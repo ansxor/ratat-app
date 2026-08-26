ca1e9ec369a3477b79c23652961b57e40958d989
darienreese <git@darienreese.ca>
2026-08-26 18:47:30 -0400

    Drive search expansion from focus

--- apps/web/src/components/search/SearchBar.tsx
+++ apps/web/src/components/search/SearchBar.tsx
@@ -8,8 +8,7 @@ import { SearchField } from "#/components/search/SearchField.tsx";
 export function SearchBar() {
   return (
     <SearchField
-      className="relative shrink-0 w-[340px] transition-[width] duration-[260ms] ease-out motion-reduce:transition-none max-[880px]:w-full max-[880px]:min-w-0"
-      openClassName="!w-[460px] max-[880px]:!w-full"
+      className="relative shrink-0 w-[340px] transition-[width] duration-[260ms] ease-out focus-within:w-[460px] motion-reduce:transition-none max-[880px]:w-full max-[880px]:min-w-0 max-[880px]:focus-within:w-full"
     />
   );
 }
